9657247dafacbbeaea7b3568e273d992c3b9280e
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-04 13:19:10 -0400

    Finish testing for allow underscore prefix
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ tests/Rules/CamelCasePropertyName/AllowUnderscorePrefixTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orrison\MessedUpPhpstan\Tests\Rules\CamelCasePropertyName;
+
+use Orrison\MessedUpPhpstan\Rules\CamelCasePropertyName\CamelCasePropertyNameRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<CamelCasePropertyNameRule>
+ */
+class AllowUnderscorePrefixTest extends RuleTestCase
+{
+    public function testExampleClass(): void
+    {
+        $this->analyse([
+            __DIR__ . '/Fixture/ExampleClass.php',
+        ], [
+            ['Property name "is_http_response" is not in camelCase.', 9],
+            ['Property name "IsHttpResponse" is not in camelCase.', 11],
+            ['Property name "isHTTPResponse" is not in camelCase.', 13],
+            ['Property name "IsHTTPResponse" is not in camelCase.', 15],
+            ['Property name "ISHTTPRESPONSE" is not in camelCase.', 17],
+            ['Property name "_isHTTPResponse" is not in camelCase.', 21],
+        ]);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/config/allow_underscore_prefix.neon'];
+    }
+
+    protected function getRule(): Rule
+    {
+        return self::getContainer()->getByType(CamelCasePropertyNameRule::class);
+    }
+}
