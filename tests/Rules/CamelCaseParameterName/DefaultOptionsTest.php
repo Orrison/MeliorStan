9199c56875ff50118350e7445dcb85e4859ce110
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-10 00:42:42 -0400

    Finish the default test
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/Rules/CamelCaseParameterName/DefaultOptionsTest.php
+++ tests/Rules/CamelCaseParameterName/DefaultOptionsTest.php
@@ -33,6 +33,30 @@ class DefaultOptionsTest extends RuleTestCase
             ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 20],
             ['Parameter name "_isHttpResponse" is not in camelCase.', 21],
             ['Parameter name "_isHTTPResponse" is not in camelCase.', 22],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 29],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 30],
+            ['Parameter name "isHTTPResponse" is not in camelCase.', 31],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 32],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 33],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 34],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 35],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 40],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 41],
+            ['Parameter name "isHTTPResponse" is not in camelCase.', 42],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 43],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 44],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 45],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 46],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 51],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 52],
+            ['Parameter name "isHTTPResponse" is not in camelCase.', 53],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 54],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 55],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 56],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 57],
         ]);
     }
 
--- tests/Rules/CamelCaseParameterName/Fixture/ExampleClass.php
+++ tests/Rules/CamelCaseParameterName/Fixture/ExampleClass.php
@@ -21,3 +21,39 @@ $testAnonFunction = function (
     string $_isHttpResponse,
     string $_isHTTPResponse,
 ): void {};
+
+class ExampleClass
+{
+    public function testMethodName(
+        string $isHttpResponse,
+        string $is_http_response,
+        string $IsHttpResponse,
+        string $isHTTPResponse,
+        string $IsHTTPResponse,
+        string $ISHTTPRESPONSE,
+        string $_isHttpResponse,
+        string $_isHTTPResponse,
+    ): void {}
+
+    protected function protectedTestMethodName(
+        string $isHttpResponse,
+        string $is_http_response,
+        string $IsHttpResponse,
+        string $isHTTPResponse,
+        string $IsHTTPResponse,
+        string $ISHTTPRESPONSE,
+        string $_isHttpResponse,
+        string $_isHTTPResponse,
+    ): void {}
+
+    private function privateTestMethodName(
+        string $isHttpResponse,
+        string $is_http_response,
+        string $IsHttpResponse,
+        string $isHTTPResponse,
+        string $IsHTTPResponse,
+        string $ISHTTPRESPONSE,
+        string $_isHttpResponse,
+        string $_isHTTPResponse,
+    ): void {}
+}
