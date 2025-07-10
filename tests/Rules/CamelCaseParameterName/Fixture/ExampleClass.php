a0abac8ea701badd56a76c0230fdae934408ec05
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-10 00:37:56 -0400

    Update test for anonymous closures
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/Rules/CamelCaseParameterName/Fixture/ExampleClass.php
+++ tests/Rules/CamelCaseParameterName/Fixture/ExampleClass.php
@@ -10,3 +10,14 @@ function testFunctionName(
     string $_isHttpResponse,
     string $_isHTTPResponse,
 ): void {}
+
+$testAnonFunction = function (
+    string $isHttpResponse,
+    string $is_http_response,
+    string $IsHttpResponse,
+    string $isHTTPResponse,
+    string $IsHTTPResponse,
+    string $ISHTTPRESPONSE,
+    string $_isHttpResponse,
+    string $_isHTTPResponse,
+): void {};
