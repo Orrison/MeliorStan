43fc68b531f9a5db8117ffc0bd3aa766ca1d224a
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-10 00:36:12 -0400

    Start the first bits of testing
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ tests/Rules/CamelCaseParameterName/DefaultOptionsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Orrison\MessedUpPhpstan\Tests\Rules\CamelCaseParameterName;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use Orrison\MessedUpPhpstan\Rules\CamelCaseParameterName\CamelCaseParameterNameRule;
+
+/**
+ * @extends RuleTestCase<CamelCaseParameterNameRule>
+ */
+class DefaultOptionsTest extends RuleTestCase
+{
+    public function testExampleClass(): void
+    {
+        $this->analyse([
+            __DIR__ . '/Fixture/ExampleClass.php',
+        ], [
+            ['Parameter name "is_http_response" is not in camelCase.', 5],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 6],
+            ['Parameter name "isHTTPResponse" is not in camelCase.', 7],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 8],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 9],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 10],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 11],
+        ]);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/config/configured_rule.neon'];
+    }
+
+    protected function getRule(): Rule
+    {
+        return self::getContainer()->getByType(CamelCaseParameterNameRule::class);
+    }
+}
---
+++ tests/Rules/CamelCaseParameterName/Fixture/ExampleClass.php
@@ -0,0 +1,12 @@
+<?php
+
+function testFunctionName(
+    string $isHttpResponse,
+    string $is_http_response,
+    string $IsHttpResponse,
+    string $isHTTPResponse,
+    string $IsHTTPResponse,
+    string $ISHTTPRESPONSE,
+    string $_isHttpResponse,
+    string $_isHTTPResponse,
+): void {}
