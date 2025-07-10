6d579034bd35ac77b6dfef17c5c52b056035fd62
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-10 09:21:28 -0400

    Finish the tests
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/Rules/CamelCaseParameterName/AllOptionsTrueTest.php
+++ tests/Rules/CamelCaseParameterName/AllOptionsTrueTest.php
@@ -15,14 +15,33 @@ class AllOptionsTrueTest extends RuleTestCase
 {
     public function testExampleClass(): void
     {
-        $this->markTestSkipped('must be revisited.');
         $this->analyse([
             __DIR__ . '/Fixture/ExampleClass.php',
         ], [
-            ['Parameter name "is_http_response" is not in camelCase.', 9],
-            ['Parameter name "IsHttpResponse" is not in camelCase.', 11],
-            ['Parameter name "IsHTTPResponse" is not in camelCase.', 15],
-            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 17],
+            ['Parameter name "is_http_response" is not in camelCase.', 5],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 6],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 8],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 9],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 16],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 17],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 19],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 20],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 29],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 30],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 32],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 33],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 40],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 41],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 43],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 44],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 51],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 52],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 54],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 55],
         ]);
     }
 
--- tests/Rules/CamelCaseParameterName/AllowConsecutiveUppercaseTest.php
+++ tests/Rules/CamelCaseParameterName/AllowConsecutiveUppercaseTest.php
@@ -16,16 +16,43 @@ class AllowConsecutiveUppercaseTest extends RuleTestCase
 {
     public function testExampleClass(): void
     {
-        $this->markTestSkipped('must be revisited.');
         $this->analyse([
             __DIR__ . '/Fixture/ExampleClass.php',
         ], [
-            ['Property name "is_http_response" is not in camelCase.', 9],
-            ['Property name "IsHttpResponse" is not in camelCase.', 11],
-            ['Property name "IsHTTPResponse" is not in camelCase.', 15],
-            ['Property name "ISHTTPRESPONSE" is not in camelCase.', 17],
-            ['Property name "_isHttpResponse" is not in camelCase.', 19],
-            ['Property name "_isHTTPResponse" is not in camelCase.', 21],
+            ['Parameter name "is_http_response" is not in camelCase.', 5],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 6],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 8],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 9],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 10],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 11],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 16],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 17],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 19],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 20],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 21],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 22],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 29],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 30],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 32],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 33],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 34],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 35],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 40],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 41],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 43],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 44],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 45],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 46],
+
+            ['Parameter name "is_http_response" is not in camelCase.', 51],
+            ['Parameter name "IsHttpResponse" is not in camelCase.', 52],
+            ['Parameter name "IsHTTPResponse" is not in camelCase.', 54],
+            ['Parameter name "ISHTTPRESPONSE" is not in camelCase.', 55],
+            ['Parameter name "_isHttpResponse" is not in camelCase.', 56],
+            ['Parameter name "_isHTTPResponse" is not in camelCase.', 57],
         ]);
     }
 
