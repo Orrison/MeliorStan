e7257c9e815d0692381f8b741a5cb178fb5da2da
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-10 00:19:20 -0400

    Setup the rule
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ src/Rules/CamelCaseParameterName/CamelCaseParameterNameRule.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Orrison\MessedUpPhpstan\Rules\CamelCaseParameterName;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Param;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<Param>
+ */
+final class CamelCaseParameterNameRule implements Rule
+{
+    protected string $pattern;
+
+    public function __construct(
+        protected Config $config,
+    ) {
+        $this->pattern = $this->buildRegexPattern();
+    }
+
+    /**
+     * @return class-string<Node>
+     */
+    public function getNodeType(): string
+    {
+        return Param::class;
+    }
+
+    /**
+     * @return RuleError[] errors
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $messages = [];
+
+        $var = $node->var;
+
+        if (! $var instanceof Variable) {
+            return $messages;
+        }
+
+        $name = is_string($var->name) ? $var->name : null;
+
+        if ($name !== null && ! preg_match($this->pattern, $name)) {
+            $messages[] = RuleErrorBuilder::message(
+                sprintf('Parameter name "%s" is not in camelCase.', $name)
+            )->build();
+        }
+
+        return $messages;
+    }
+
+    protected function buildRegexPattern(): string
+    {
+        $pattern = '/^';
+
+        $pattern .= $this->config->getAllowUnderscorePrefix()
+            ? '_?'
+            : '';
+
+        $pattern .= '[a-z]';
+
+        $pattern .= $this->config->getAllowConsecutiveUppercase()
+            ? '[a-zA-Z0-9]*'
+            : '(?:[a-z0-9]+|[A-Z][a-z0-9]+)*';
+
+        $pattern .= '$/';
+
+        return $pattern;
+    }
+}
