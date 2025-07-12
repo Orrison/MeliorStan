0f519d1d1d0e1d75de0d765750aa7acf72da8e52
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-12 15:09:48 -0400

    Fix tests
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/Rules/Superglobals/DefaultOptionsTest.php
+++ tests/Rules/Superglobals/DefaultOptionsTest.php
@@ -18,18 +18,18 @@ class DefaultOptionsTest extends RuleTestCase
         $this->analyse([
             __DIR__ . '/Fixture/ExampleClass.php',
         ], [
-            ['Superglobal "$_SERVER" should not be used in userland code.', 10],
-            ['Superglobal "$_GET" should not be used in userland code.', 11],
-            ['Superglobal "$_POST" should not be used in userland code.', 12],
-            ['Superglobal "$_FILES" should not be used in userland code.', 13],
-            ['Superglobal "$_COOKIE" should not be used in userland code.', 14],
-            ['Superglobal "$_SESSION" should not be used in userland code.', 15],
-            ['Superglobal "$_REQUEST" should not be used in userland code.', 16],
-            ['Superglobal "$_ENV" should not be used in userland code.', 17],
-            ['Superglobal "$GLOBALS" should not be used in userland code.', 18],
-            ['Superglobal "$_GET" should not be used in userland code.', 33],
-            ['Superglobal "$_POST" should not be used in userland code.', 34],
-            ['Superglobal "$_COOKIE" should not be used in userland code.', 37],
+            ['Superglobal "$_SERVER" should not be used.', 10],
+            ['Superglobal "$_GET" should not be used.', 11],
+            ['Superglobal "$_POST" should not be used.', 12],
+            ['Superglobal "$_FILES" should not be used.', 13],
+            ['Superglobal "$_COOKIE" should not be used.', 14],
+            ['Superglobal "$_SESSION" should not be used.', 15],
+            ['Superglobal "$_REQUEST" should not be used.', 16],
+            ['Superglobal "$_ENV" should not be used.', 17],
+            ['Superglobal "$GLOBALS" should not be used.', 18],
+            ['Superglobal "$_GET" should not be used.', 33],
+            ['Superglobal "$_POST" should not be used.', 34],
+            ['Superglobal "$_COOKIE" should not be used.', 37],
         ]);
     }
 
