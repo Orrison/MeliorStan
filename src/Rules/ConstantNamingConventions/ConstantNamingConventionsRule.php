c4add72172d1f950075e25d414ad9d4284729557
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-07-27 17:37:11 -0400

    Create ConstantNamingConventionsRule
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ src/Rules/ConstantNamingConventions/ConstantNamingConventionsRule.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Orrison\MessedUpPhpstan\Rules\ConstantNamingConventions;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassConst;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<ClassConst>
+ */
+final class ConstantNamingConventionsRule implements Rule
+{
+    /**
+     * @return class-string<Node>
+     */
+    public function getNodeType(): string
+    {
+        return ClassConst::class;
+    }
+
+    /**
+     * @return RuleError[] errors
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        $messages = [];
+
+        foreach ($node->consts as $const) {
+            $name = $const->name->name;
+
+            // Check if constant name is all uppercase
+            if ($name !== strtoupper($name)) {
+                $messages[] = RuleErrorBuilder::message(
+                    sprintf('Constant name "%s" is not in UPPERCASE.', $name)
+                )->identifier('MessedUpPhpstan.constantNamingConventions')
+                    ->build();
+            }
+        }
+
+        return $messages;
+    }
+}
---
+++ tests/Rules/ConstantNamingConventions/Fixture/InterfaceEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orrison\MessedUpPhpstan\Tests\Rules\ConstantNamingConventions\Fixture;
+
+interface ExampleInterface
+{
+    public const INTERFACE_CONSTANT = 'valid';
+
+    public const interfaceConstant = 'invalid';
+}
+
+enum ExampleEnum
+{
+    public const ENUM_CONSTANT = 'valid';
+
+    public const enumConstant = 'invalid';
+    case ValidCase;
+    case invalid_case;
+}
