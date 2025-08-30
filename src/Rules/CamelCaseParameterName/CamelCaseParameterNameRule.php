57c1175f2e4165d63d5f339b5f05bac022bb8687
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-08-30 13:41:11 -0400

    More fixes
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- src/Rules/CamelCaseParameterName/CamelCaseParameterNameRule.php
+++ src/Rules/CamelCaseParameterName/CamelCaseParameterNameRule.php
@@ -67,7 +67,7 @@ class CamelCaseParameterNameRule implements Rule
 
         $pattern .= $this->config->getAllowConsecutiveUppercase()
             ? '[a-zA-Z0-9]*'
-            : '(?:[a-z0-9]*[A-Z](?![A-Z])[a-z0-9]*)*';
+            : '(?:[a-z0-9]+|[a-z0-9]*[A-Z](?![A-Z])[a-z0-9]*)*';
 
         $pattern .= '$/';
 
