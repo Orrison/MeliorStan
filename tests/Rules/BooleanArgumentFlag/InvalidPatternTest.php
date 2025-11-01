b0b411bf377a8d5d310b6c25fafad076684ef299
Kevin Ullyott <ullyott.kevin@gmail.com>
2025-11-01 15:35:49 -0400

    Add tests for InvalidArgumentException in BooleanArgumentFlagRule and include invalid regex configuration
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

---
+++ tests/Rules/BooleanArgumentFlag/InvalidPatternTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Orrison\MeliorStan\Tests\Rules\BooleanArgumentFlag;
+
+use InvalidArgumentException;
+use Orrison\MeliorStan\Rules\BooleanArgumentFlag\BooleanArgumentFlagRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<BooleanArgumentFlagRule>
+ */
+class InvalidPatternTest extends RuleTestCase
+{
+    public function testInvalidPattern(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid regex pattern in ignore_pattern configuration');
+
+        $this->analyse([
+            __DIR__ . '/Fixture/IgnorePattern.php',
+        ], []);
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [__DIR__ . '/config/invalid-pattern.neon'];
+    }
+
+    protected function getRule(): Rule
+    {
+        return self::getContainer()->getByType(BooleanArgumentFlagRule::class);
+    }
+}
